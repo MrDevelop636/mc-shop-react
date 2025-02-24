5a2ef3dbbd09c5e14711e6813a5abe874711e8dd
ZLMTech <mattimusicpl@gmail.com>
2025-02-24 22:36:27 +0100

    up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
 import "./assets/css/main.css"; // Importowanie stylów
+import HeroSection from "./components/Hero";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      <HeroSection />
       <Products />
       <Footer />
     </div>
